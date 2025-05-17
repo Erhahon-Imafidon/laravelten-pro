0cadf3df21ebbffbfce2549eab24236c11d74e3c
Erhahon Imafidon <mafiosco4@gmail.com>
2025-05-17 13:50:35 +0100

    formatted the two blade files I have in my project

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,13 +1,19 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>User</title>
+   <meta charset="UTF-8">
+   <meta
+      name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+   >
+   <meta
+      http-equiv="X-UA-Compatible"
+      content="ie=edge"
+   >
+   <title>User</title>
 </head>
 <body>
-<h1>User View</h1>
-    <h2>User Name {{$name}}</h2>
+   <h1>User View</h1>
+   <h2>User Name {{ $name }}</h2>
 </body>
 </html>
