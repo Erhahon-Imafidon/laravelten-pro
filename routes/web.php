6df70f64252c81431df5f10c20013b89b4cd4e47
Erhahon Imafidon <mafiosco4@gmail.com>
2025-05-19 00:46:34 +0100

    created a client controller to get all post and also by id

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
@@ -21,3 +22,7 @@ Route::get('/', [ProductController::class, 'index'])->name('product.index');
 Route::get('/home/{name?}/',[HomeController::class, 'index'])->name('home.index');
 
 Route::get('/user', [UserController::class, 'index'])->name('user.index');
+
+Route::get('/posts', [ClientController::class, 'getAllPost'])->name('posts.getallpost');
+
+Route::get('/posts/{id}', [ClientController::class, 'getPostById'])->name('posts.getpostbyid');
