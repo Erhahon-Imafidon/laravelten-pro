9020dcdeff21a86af338650b0d663cb17f6a21c2
Erhahon Imafidon <mafiosco4@gmail.com>
2025-05-27 12:38:50 +0100

    Add an icon to show and hide password

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,6 +13,10 @@ class LoginController extends Controller
 
     public function loginSubmit(Request $request)
     {
+        $validatedData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:20'
+        ]);
         $email = $request->input('email');
         $password = $request->input('password');
         return "Email: ". $email . ", Password: " . $password;
