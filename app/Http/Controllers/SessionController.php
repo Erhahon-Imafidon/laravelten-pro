38c7e5ccca78081d96f108e6054a773aa6110ced
Erhahon Imafidon <mafiosco4@gmail.com>
2025-05-28 14:13:09 +0100

    created sessions to get, add, and delete data in a session

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -6,8 +6,27 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-    public function create()
+    public function getSessionData(Request $request)
     {
+        if($request->session()->has('name'))
+        {
+            echo $request->session()->get('name');
+        }
+        else
+        {
+          echo 'Session data not found.';
+        }
+    }
 
+    public function storeSessionData(Request $request)
+    {
+        $request->session()->put('name', 'Imafidon');
+        echo 'Data has been added to the session.';
+    }
+
+    public function deleteSessionData(Request $request)
+    {
+         $request->session()->forget('name');
+         echo 'Session data has been deleted.';
     }
 }
