efdaa685e9d04071970202d2382e8f07e420c127
Erhahon Imafidon <mafiosco4@gmail.com>
2025-05-30 11:37:31 +0100

    design the table element and put the posts inside the table

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,16 +8,32 @@
     <title>Posts</title>
 </head>
 <body class="font-figtree">
-<main>
-    <h1 class="text-4xl font-bold mb-2">All Posts</h1>
-    <div class="flex flex-col gap-4">
-        @foreach($posts as $post)
-            <div class="">
-                <h2 class="font-bold text-2xl">{{$post->title}}</h2>
-                <p>{{$post->body}}</p>
+<main class="py-8">
+    <section class="container">
+        <div class="w-full ">
+            <div class="flex px-6 py-4 text-xl font-medium">
+                <h1 class="text-2xl">All Posts</h1>
             </div>
-        @endforeach
-    </div>
+            <div>
+                <table class="table-auto w-full">
+                    <thead class="bg-gray-100 text-left p-4 text-xl">
+                        <tr>
+                            <th class="p-4 border-b border-gray-300">Post Title</th>
+                            <th class="p-4 border-b border-gray-300">Post Body</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($posts as $post)
+                            <tr>
+                                <td class="border-b border-gray-300 p-4">{{ $post->title }}</td>
+                                <td class="border-b border-gray-300 p-4">{{ $post->body }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
 </main>
 </body>
 </html>
